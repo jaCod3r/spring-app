3daa036a1a6817a4fa3e6480749a4a9ce7827ebe
Juber Ahmed <a.juber@hotmail.com>
2018-08-20 02:04:28 -0400

    Added Id to model. Closes #10

--- app-data/src/main/java/app/ajuber/springapp/model/Pet.java
+++ app-data/src/main/java/app/ajuber/springapp/model/Pet.java
@@ -2,7 +2,7 @@ package app.ajuber.springapp.model;
 
 import java.time.LocalDate;
 
-public class Pet extends PetType {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
