dbf1a6a07700aa7b3d787d38f44c52dd55c7a4fa
Juber Ahmed <a.juber@hotmail.com>
2018-08-21 00:40:04 -0400

    Refactored to common crud interface. closes #11

--- app-data/src/main/java/app/ajuber/springapp/services/VetService.java
+++ app-data/src/main/java/app/ajuber/springapp/services/VetService.java
@@ -2,14 +2,7 @@ package app.ajuber.springapp.services;
 
 import app.ajuber.springapp.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
 
