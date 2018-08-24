6876582227cec171b3a5fa8d4e4bbfbab43afc0d
Juber Ahmed <a.juber@hotmail.com>
2018-08-23 23:59:38 -0400

    Updated services

--- app-data/src/main/java/app/ajuber/springapp/services/OwnerService.java
+++ app-data/src/main/java/app/ajuber/springapp/services/OwnerService.java
@@ -3,7 +3,7 @@ package app.ajuber.springapp.services;
 import app.ajuber.springapp.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
-
+    //updated
     Owner findByLastName(String lastName);
 
 }
